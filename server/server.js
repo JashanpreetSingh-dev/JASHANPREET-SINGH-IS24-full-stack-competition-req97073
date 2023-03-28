881765d3ac7bb5aa72567f3cf84aeb7569d9a39b
JashanpreetSingh-dev <77453449+JashanpreetSingh-dev@users.noreply.github.com>
2023-03-27 21:24:13 -0700

    Added the PUT route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const options = {
         openapi: '3.0.0',
         info: {
             title: 'Product API',
+            description: 'This API is developed for IS24 Full Stack Developer Coding Challenge from BC Ministry of Citizens Services - Information Management Branch by Jashanpreet Singh',
             version: '1.0.0',
         },
     },
@@ -81,41 +82,57 @@ function filterBy(req, res, products, queryParam, propName) {
  * @swagger
  * /api/products:
  *   get:
- *     summary: Get a list of products
+ *     summary: Retrieve a list of products
+ *     description: Retrieve a list of products with optional filters. If no filters are specified, all products are returned.
+ *     tags:
+ *       - Products
  *     parameters:
  *       - in: query
  *         name: productOwner
- *         description: Filter by product owner
- *         required: false
  *         schema:
  *           type: string
+ *         description: Filter by product owner
  *       - in: query
  *         name: developerName
- *         description: Filter by developer name
- *         required: false
  *         schema:
  *           type: string
+ *         description: Filter by developer name
  *       - in: query
  *         name: scrumMaster
- *         description: Filter by scrum master
- *         required: false
  *         schema:
  *           type: string
+ *         description: Filter by scrum master
  *       - in: query
  *         name: methodology
- *         description: Filter by methodology
- *         required: false
  *         schema:
  *           type: string
+ *         description: Filter by methodology
  *     responses:
- *       200:
- *         description: A list of products
+ *       '200':
+ *         description: A list of products that match the filters
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Product'
+ *               $ref: '#/components/schemas/Product'
+ *             example:
+ *               - productNumber: 1
+ *                 productName: "Product 1"
+ *                 productOwner: "Isabelle"
+ *                 developers: ["Olivia","Peter","Cathy","Jake"]
+ *                 scrumMaster: "Fiona"
+ *                 startDate: "2023-03-27T01:56:31.384Z"
+ *                 methodology: "Agile"
+ *               - productNumber: 2
+ *                 productName: "Product 2"
+ *                 productOwner: "Henry"
+ *                 developers: ["Tina"]
+ *                 scrumMaster: "Ian"
+ *                 startDate: "2023-03-27T01:56:31.384Z"
+ *                 methodology: "Waterfall"
+ *       '400':
+ *         description: Bad request. Invalid request parameters.
+ *       '500':
+ *         description: Internal server error. Failed to read from data file.
  */
 app.get('/api/products', (req, res) => {
     // res.send(products);
@@ -136,55 +153,36 @@ app.get('/api/products', (req, res) => {
  * @swagger
  * /api/product/{productNumber}:
  *   get:
- *     summary: Get a product by product number
+ *     summary: Get a product by its product number
+ *     description: Retrieve a product from the system by its unique product number
+ *     tags:
+ *       - Products
  *     parameters:
  *       - in: path
  *         name: productNumber
- *         schema:
- *           type: integer
  *         required: true
  *         description: Numeric ID of the product to retrieve
+ *         schema:
+ *           type: integer
  *     responses:
  *       '200':
- *         description: A product object
+ *         description: Product found and returned
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 productNumber:
- *                   type: integer
- *                   description: The unique identifier for the product
- *                   example: 12345
- *                 productName:
- *                   type: string
- *                   description: The name of the product
- *                   example: "Example Product"
- *                 productOwner:
- *                   type: string
- *                   description: The name of the product owner
- *                   example: "John Doe"
- *                 developers:
- *                   type: array
- *                   items:
- *                     type: string
- *                   description: The list of developers working on the product
- *                   example: ["Alice", "Bob"]
- *                 scrumMaster:
- *                   type: string
- *                   description: The name of the scrum master for the product
- *                   example: "Jane Doe"
- *                 startDate:
- *                   type: string
- *                   format: date-time
- *                   description: The start date of the product development
- *                   example: "2022-01-01T00:00:00.000Z"
- *                 methodology:
- *                   type: string
- *                   description: The development methodology used for the product
- *                   example: "Agile"
+ *               $ref: '#/components/schemas/Product'
+ *             example:
+ *               productNumber: 41
+ *               productName: "Product 41"
+ *               productOwner: "John Doe"
+ *               developers: ["Jane Doe", "Bob Smith"]
+ *               scrumMaster: "Mary Johnson"
+ *               startDate: "2023-03-26T10:00:00.000Z"
+ *               methodology: "Agile"
  *       '404':
- *         description: Product not found
+ *         description: The product with the given ID was not found.
+ *       '500':
+ *         description: Internal server error. Failed to read data file.
  */
 app.get('/api/product/:productNumber', (req, res) => {
     const products = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
@@ -260,6 +258,78 @@ app.post('/api/product', (req, res) => {
         methodology: req.body.methodology
     };
     products.push(product);
+    fs.writeFileSync('./data.json', JSON.stringify(products));
+    res.send(product);
+});
+
+/**
+ * @swagger
+ * /api/product/{productNumber}:
+ *   put:
+ *     summary: Update an existing product
+ *     description: Update an existing product in the system
+ *     tags:
+ *       - Products
+ *     parameters:
+ *       - in: path
+ *         name: productNumber
+ *         description: ID of the product to update
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       description: Product object that needs to be updated
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Product'
+ *           example:
+ *             productName: "Product 41"
+ *             productOwner: "John Doe"
+ *             developers: ["Jane Doe", "Bob Smith"]
+ *             scrumMaster: "Mary Johnson"
+ *             startDate: "2023-03-26T10:00:00.000Z"
+ *             methodology: "Agile"
+ *     responses:
+ *       '200':
+ *         description: Product successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *             example:
+ *               productNumber: 41
+ *               productName: "Product 41"
+ *               productOwner: "John Doe"
+ *               developers: ["Jane Doe", "Bob Smith"]
+ *               scrumMaster: "Mary Johnson"
+ *               startDate: "2023-03-26T10:00:00.000Z"
+ *               methodology: "Agile"
+ *       '400':
+ *         description: Bad request. Invalid request body or product name already exists.
+ *       '404':
+ *         description: The product with the given ID was not found.
+ *       '500':
+ *         description: Internal server error. Failed to write to data file.
+ */
+app.put('/api/product/:productNumber', (req, res) => {
+    const product = products.find(p => p.productNumber === parseInt(req.params.productNumber));
+    if (!product) res.status(404).send('The product with the given ID was not found.');
+
+    const { error } = productSchema.validate(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    product.productName = req.body.productName;
+    product.productOwner = req.body.productOwner;
+    product.developers = req.body.developers;
+    product.scrumMaster = req.body.scrumMaster;
+    product.startDate = req.body.startDate;
+    product.methodology = req.body.methodology;
     fs.writeFileSync('./data.json', JSON.stringify(products));
     res.send(product);
 });
